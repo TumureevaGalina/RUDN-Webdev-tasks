b1c89b4d193fdf11228cae033fe305a60d2249a6
TumureevaGalina <tumureeva05@mail.ru>
2026-01-15 15:31:53 +0300

    3.1-tasks

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import EnergyFormula from "./components/EnergyFormula";
+import PythagoreanTheorem from "./components/PythagoreanTheorem";
+import CylinderVolume from "./components/CylinderVolume";
+import QuadraticEquation from "./components/QuadraticEquation";
+import SquareOfSum from "./components/SquareOfSum";
+import WaterFormula from "./components/WaterFormula";
+import Logarithm from "./components/Logarithm";
+
+function App() {
+  return (
+    <div>
+      <h2>Формулы для вёрстки:</h2>
+      <ol>
+        <EnergyFormula />
+        <PythagoreanTheorem />
+        <CylinderVolume />
+        <QuadraticEquation />
+        <SquareOfSum />
+        <WaterFormula />
+        <Logarithm />
+      </ol>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/PythagoreanTheorem.tsx
@@ -0,0 +1,5 @@
+function PythagoreanTheorem() {
+  return <li>Теорема Пифагора: a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup></li>;
+}
+
+export default PythagoreanTheorem;
